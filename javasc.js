0da02d3086f1ae395d8231264cb05cf76858b199
Sophia07-fun <71560924+Sophia07-fun@users.noreply.github.com>
2022-09-04 09:55:50 +0200

    Add files via upload

---
+++ javasc.js
@@ -0,0 +1,50 @@
+let color1_input = document.getElementById("color1")
+let color2_input = document.getElementById("color2")
+let color3_input = document.getElementById("color3")
+let color4_input = document.getElementById("color4")
+let color5_input = document.getElementById("color5")
+let color6_input = document.getElementById("color6")
+let color7_input = document.getElementById("color7")
+let color8_input = document.getElementById("color8")
+let color9_input = document.getElementById("color9")
+let color10_input = document.getElementById("color10")
+let color11_input = document.getElementById("color11")
+let color12_input = document.getElementById("color12")
+
+let color1_output = document.getElementById("0color1")
+let color2_output = document.getElementById("0color2")
+let color3_output = document.getElementById("0color3")
+let color4_output = document.getElementById("0color4")
+let color5_output = document.getElementById("0color5")
+let color6_output = document.getElementById("0color6")
+let color7_output = document.getElementById("0color7")
+let color8_output = document.getElementById("0color8")
+let color9_output = document.getElementById("0color9")
+let color10_output = document.getElementById("0color10")
+let color11_output = document.getElementById("0color11")
+let color12_output = document.getElementById("0color12")
+
+color1_input.addEventListener("click", ()=>{
+  console.log(color1)})
+color2_input.addEventListener("click", ()=>{
+  console.log(color2)})
+color3_input.addEventListener("click", ()=>{
+  console.log(color3)})
+color4_input.addEventListener("click", ()=>{
+  console.log(color4)})
+color5_input.addEventListener("click", ()=>{
+  console.log(color5)})
+color6_input.addEventListener("click", ()=>{
+  console.log(color6)})
+color7_input.addEventListener("click", ()=>{
+  console.log(color7)})
+color8_input.addEventListener("click", ()=>{
+  console.log(color8)})
+color9_input.addEventListener("click", ()=>{
+  console.log(color9)})
+color10_input.addEventListener("click", ()=>{
+  console.log(color10)})
+color11_input.addEventListener("click", ()=>{
+  console.log(color11)})
+color12_input.addEventListener("click", ()=>{
+  console.log(color12)})
